4467ab9bf9c3a4aa4958bb47d62ca126eef3d4fd
Reham-omar <rehamalsobh96@gmail.com>
2020-02-05 21:51:50 +0200

    a

--- java.js
+++ java.js
@@ -8,15 +8,28 @@ var person = prompt("Please enter your name", "Reham Omar");
 
 function myFunction() {
   var txt;
-  var r = prompt("Are you from jordan ?");
+  var r = prompt("Are you from jordan ? (yes or No)");
   if (r == "yes") {
-    person =confirm ("I'm from jordan too ?");
-  } else {
+    person =confirm ("I'm from jordan too ");
+  } else if (r=="No"){
     person = confirm ("Welcome to jordan ");
-  }
-  /*console.log(txt)*/
-  /*document.getElementById("demo").innerHTML = txt;*/
-}
-myFunction ();
+  } else while(r!=="yes" && r!=="No" )
+  { 
+    var r = prompt("Are you from jordan ? (yes or No)");
 
- 
+  }
+  var n = prompt("How many times  did you visit petra ?");
+var item="";
+for(var i=0;i<n;i++)
+{
+  item=item+ '<img src="1111.jpg">';
+}
+  return item;
+
+  }
+//myFunction ();
+
+document.write('<p>'+myFunction()+'</p>');
+
+ /*console.log(txt)*/
+  /*document.getElementById("demo").innerHTML = txt;*/
