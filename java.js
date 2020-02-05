fa27ea8835e7a7f8c4ff43aa643b2c4df22d280f
Reham-omar <rehamalsobh96@gmail.com>
2020-02-05 14:12:34 +0200

    v

--- java.js
+++ java.js
@@ -1,36 +1,21 @@
+var person = prompt("Please enter your name", "Reham Omar");
 
+ if (person != null) {
+  document.getElementById("drink").innerHTML =
+   "Hello " + person + "! How are you today?";
+ }
 
 function myFunction() {
   var txt;
-  var r = confirm("Press a button!");
+  var r = confirm("Are you from jordan ?");
   if (r == true) {
-    txt = "You pressed OK!";
+    txt = "I really love jordan";
   } else {
-    txt = "You pressed Cancel!";
+    txt = "welcome to jordan";
   }
+  console.log(txt)
   document.getElementById("demo").innerHTML = txt;
 }
+myFunction ();
 
-var person = prompt("Please enter your name", "Reham Omar");
-
-if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
-}
-
-var text;
-var favDrink = prompt("What's your favorite  drink?");
-switch(favDrink) {
-  case "orange":
-    text = "Excellent choice! it is good for your soul.";
-    break;
-  case "tea":
-    text = "tea is my favorite too!";
-    break;
-  case "coffee":
-    text = "Really? Are you sure the coffee is your favorite?";
-    break;
-  default:
-    text = "I have never heard of that one..";
-    break;
-}
+ 
